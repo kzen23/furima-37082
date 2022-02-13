35802ad3fcba153c96c3e356bd98b841eb83cfd5
kzen23 <kn0223ani0824@gmail.com>
2022-02-13 22:18:09 +0900

    アソシエーション
    userモデルとitemモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers"}
 
   with_options presence: true do
@@ -20,4 +22,6 @@ class User < ApplicationRecord
       validates :first_name_kana
     end
   end
+
+
 end
