f72f9eeba394ce636f5fc9b9ff87a91799c2e6fd
kzen23 <kn0223ani0824@gmail.com>
2022-02-15 21:09:13 +0900

    Active Hash導入・作成・記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
 end
