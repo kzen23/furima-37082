2857058a06a3e99f8ac6c46ef858199eb3ac260d
kzen23 <kn0223ani0824@gmail.com>
2022-02-16 16:41:40 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,20 @@ class Item < ApplicationRecord
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
+
+  with_options presence: true do
+    :name
+    :info
+    :image
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    :categroy_id
+    :sales_status
+    :shipping_fee_status
+    :scheduled_delivery
+  end
+
+  validates :prefecture, numericality: { other_than: 0, message: "can't be blank"}
+  validates :price, numericality: { :only_integer: true, greater_than_or_equal: 300, less_than_or_equal_to: 9999999, message: "is invalid" }
 end
