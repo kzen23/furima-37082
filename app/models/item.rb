f825ed87febd0f6c0257b1afcad765c1c1918d36
kzen23 <kn0223ani0824@gmail.com>
2022-02-17 16:44:37 +0900

    出品した商品情報をテーブルに保存できるようにする

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,18 +6,19 @@ class Item < ApplicationRecord
   belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
 
   with_options presence: true do
-    :name
-    :info
-    :image
+    validates :image
+    validates :name
+    validates :info
+    validates :price, numericality: { only_integer: true, message: "is invalid. Input half width characters" }
   end
+  validates :price, numericality: { greater_than_or_equal: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
-    :categroy_id
-    :sales_status
-    :shipping_fee_status
-    :scheduled_delivery
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :scheduled_delivery_id
   end
 
-  validates :prefecture, numericality: { other_than: 0, message: "can't be blank"}
-  validates :price, numericality: { :only_integer: true, greater_than_or_equal: 300, less_than_or_equal_to: 9999999, message: "is invalid" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
 end
