427af95112713fd8ccedca3592eb2e7588b9eb5f
kzen23 <kn0223ani0824@gmail.com>
2022-02-18 22:15:34 +0900

    JavaScriptで販売手数料・利益計算結果の表示
    ユーザーの出品価格入力に応じて販売手数料・利益計算結果を表示できるようにした。

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,6 +2,10 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+    const addTax = inputValue * 0.1
+    addTaxDom.innerHTML = Math.floor(addTax)
+    profitDom.innerHTML = Math.floor(inputValue - addTax)
   })
 });
