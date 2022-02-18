63f561153eab207703ba5bb185f0708d4cb60b6e
kzen23 <kn0223ani0824@gmail.com>
2022-02-18 19:27:11 +0900

    itemモデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
     validates :info
     validates :price, numericality: { only_integer: true, message: "is invalid. Input half width characters" }
   end
-  validates :price, numericality: { greater_than_or_equal: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"テスト"}
+    info {"これはテストです。"}
+    price {Faker::Number.within(range: 300..9999999)}
+    id_num = Faker::Number.within(range: 2..3)
+    category_id {id_num}
+    sales_status_id {id_num}
+    shipping_fee_status_id {id_num}
+    scheduled_delivery_id {id_num}
+    prefecture_id {Faker::Number.within(range: 1..47)}
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
