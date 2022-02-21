c05a15af3fdb9d3ab8fbc0fcbd155daf3d3bc542
kzen23 <kn0223ani0824@gmail.com>
2022-02-21 20:59:41 +0900

    Revert "商品情報、sold outの表示"
    This reverts commit a57864189133aad1239a1d9c871577b40a282ec7.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category 
-  belongs_to :sales_status
-  belongs_to :shipping_fee_status
-  belongs_to :prefecture
-  belongs_to :scheduled_delivery
+  belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
 
   with_options presence: true do
     validates :image
