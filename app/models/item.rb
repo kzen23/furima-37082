9ed05cee01ad419e850169486570c14f79e5082f
kzen23 <kn0223ani0824@gmail.com>
2022-02-22 14:51:51 +0900

    rubocopの自動修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category 
+  belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
   belongs_to :prefecture
