745cc530d7532069ba9f2b9c8cdc5c4641dc4a54
kzen23 <kn0223ani0824@gmail.com>
2022-02-25 21:21:18 +0900

    end忘れ
    エラーが出たので、正しく修正。

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -19,4 +19,5 @@ attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building_name, :
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
