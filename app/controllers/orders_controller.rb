a009f7592ce3f6f9ba04048291752d90268ab2c8
kzen23 <kn0223ani0824@gmail.com>
2022-03-01 19:11:11 +0900

    order_controllerのリファクタリングと修正
    pay_itemメソッドをprivateで定義。
    
    modelオプションが正常に働いたためにparamsの内容も変化したので、情報を取得・保存できるように記述を変更。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,9 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-       pay_item
-       @order_address.save
-       redirect_to root_path
+      pay_item
+      @order_address.save
+      redirect_to root_path
     else
       render :index
     end
@@ -19,17 +19,17 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:postal_code, :prefecture_id, :city, :addresses, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_7c6b45717691fd72ff9a6780"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
       currency: 'jpy'
     )
   end
-  
+
 end
 
