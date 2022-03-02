3bc525a4aad02c9f81e5a5810dc3dd450049f7c0
kzen23 <94582364+kzen23@users.noreply.github.com>
2022-03-02 11:57:48 +0900

    テストコード実行
    order_addressで必要な情報とtokenについてテストコードを実行した。

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-4567' }
+    prefecture_id { Faker::Number.within(range: 1..47) }
+    city { '北海道' }
+    addresses { 'テスト1-1' }
+    building_name { 'テスト北海道' }
+    phone_number { '09012345678' }
+    token { "tok_abcdefg00000000000" }
+  end
+end
